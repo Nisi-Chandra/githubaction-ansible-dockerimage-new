b2dcb04a1b69e6994f4195e8d7ff31df2497d4f7
nisi <nisi@gmail.com>
2023-09-29 14:30:05 +0000

    flask-application-version-1

---
+++ get-vault-password.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+SECRET_NAME="ipgelocation"  # Replace with your secret name or ARN
+
+SECRET_JSON=$(aws secretsmanager get-secret-value --secret-id $SECRET_NAME)
+SECRET_VALUE=$(echo $SECRET_JSON | jq -r '.SecretString | fromjson.ansible_vault')
+
+echo "$SECRET_VALUE"
