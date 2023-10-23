edc361181a073468fe4c8ea938cb71b224eaf29c
Nisi-Chandra <134756912+Nisi-Chandra@users.noreply.github.com>
2023-10-23 10:37:55 +0530

    Update get-vault-password.sh

--- get-vault-password.sh
+++ get-vault-password.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SECRET_NAME="ipgelocation"  # Replace with your secret name or ARN
+SECRET_NAME="secret-pass"  # Replace with your secret name or ARN
 
 SECRET_JSON=$(aws secretsmanager get-secret-value --secret-id $SECRET_NAME)
 SECRET_VALUE=$(echo $SECRET_JSON | jq -r '.SecretString | fromjson.ansible_vault')
